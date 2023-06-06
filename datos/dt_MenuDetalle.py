3d68c56ae261154e0fa47a4d29888778b1892bb2
Casaya29 <106005759+Casaya29@users.noreply.github.com>
2023-06-06 12:48:24 -0600

    Add files via upload

--- datos/dt_MenuDetalle.py
+++ datos/dt_MenuDetalle.py
@@ -3,32 +3,34 @@ from .conexion import Conexion
 from entidades.menuDetalle import MenuDetalle
 
 class Dt_MenuDetalle:
-    _SELECT = 'SELECT * FROM JirehBD.MenuDetalle where estado <> 3'
-    _INSERT = 'insert into JirehBD.MenuDetalle(MenuID,Menu_tipoID,Nombre,Descripcion,Precio,Estado) values(MenuID=%s,Menu_tipoID=%s,Nombre_=%s,Descripcion=%s,Precio=%s,1)'
-    _UPDATE = "UPDATE MenuDetalle set MenuID=%s,Menu_tipoID=%s,Nombre_=%s,Descripcion=%s,Precio=%s where Menu_detalleID=%s"
-    _DELETE = "UPDATE MenuDetalle set Estado=3 where MenuDetalleID=%s"
+    _SELECT = 'SELECT * FROM JirehDB.Menu_detalle WHERE estado <> 3'
+    _INSERT = 'INSERT INTO JirehDB.Menu_detalle(MenuID, Menu_tipoID, Nombre, Descripcion, Precio, Estado) VALUES (%s, %s, %s, %s, %s, 1)'
+    _UPDATE = "UPDATE JirehDB.Menu_detalle SET MenuID=%s, Menu_tipoID=%s, Nombre=%s, Descripcion=%s, Precio=%s WHERE Menu_detalleID=%s"
+    _DELETE = "UPDATE JirehDB.Menu_detalle SET Estado=3 WHERE Menu_detalleID=%s"
+
     @classmethod
-    def listarMenuDetale(cls):
+    def listarMenuDetalle(cls):
         cursor = Conexion.getConnection().cursor()
         cursor.execute(cls._SELECT)
         resultado = cursor.fetchall()
-        MenuDetalles= []
+        MenuDetalles = []
 
         for x in resultado:
-            md = MenuDetalle(x['Menu_detalleID'], x['MenuID'], x['Menu_tipoID'] ,x['Nombre'], x['Descripcion'], x['Precio'])
-            MenuDetalle.append(md)
-            print('MenuDetalle', MenuDetalle)
-            return MenuDetalle
+            md = MenuDetalle(x['Menu_detalleID'], x['MenuID'], x['Menu_tipoID'], x['Nombre'], x['Descripcion'], x['Precio'])
+            MenuDetalles.append(md)
+            print('MenuDetalles', MenuDetalles)
+
+        return MenuDetalles
 
     @classmethod
-    def guardarMenuDetalle(cls,MenuDetalle):
+    def guardarMenuDetalle(cls, MenuDetalle):
         cursor = Conexion.getConnection().cursor()
         conexion = Conexion.getConnection()
 
         try:
             print(f'MenuDetalle al insertar: {MenuDetalle}')
-            valores = (MenuDetalle.MenuID,MenuDetalle.Menu_tipoID,MenuDetalle.Nombre,MenuDetalle.Descripcion,MenuDetalle.Precio)
-            cursor.execute(cls._INSERT,valores)
+            valores = (MenuDetalle.MenuID, MenuDetalle.Menu_tipoID, MenuDetalle.Nombre, MenuDetalle.Descripcion, MenuDetalle.Precio)
+            cursor.execute(cls._INSERT, valores)
             print(f'MenuDetalle insertado: {MenuDetalle}')
             conexion.commit()
             return cursor.rowcount
@@ -36,21 +38,20 @@ class Dt_MenuDetalle:
             print(f'Exception {e}')
 
     @classmethod
-    def actualizarMenuDetalle(cls,MenuDetalle):
+    def actualizarMenuDetalle(cls, MenuDetalle):
         cursor = Conexion.getConnection().cursor()
         conexion = Conexion.getConnection()
 
         try:
             print(f'MenuDetalle al actualizar')
-            valores = (MenuDetalle.MenuID,MenuDetalle.Menu_tipoID,MenuDetalle.Nombre,MenuDetalle.Descripcion,MenuDetalle.Precio,MenuDetalle.Menu_detalleID)
-            cursor.execute(cls._UPDATE,valores)
+            valores = (MenuDetalle.MenuID, MenuDetalle.Menu_tipoID, MenuDetalle.Nombre, MenuDetalle.Descripcion,
+                       MenuDetalle.Precio, MenuDetalle.Menu_detalleID)
+            cursor.execute(cls._UPDATE, valores)
             print(f'Actualizando MenuDetalle')
             conexion.commit()
             return cursor.rowcount
         except Exception as e:
             print(f'Exception al editar: {e.__traceback__}')
-
-
 
     @classmethod
     def eliminarMenuDetalle(cls,MenuDetalle):
@@ -65,8 +66,5 @@ class Dt_MenuDetalle:
             return cursor.rowcount
 
         except Exception as e:
-            print(f'Ocuriio un error al eliminar el Menu Detalle: {e}')
+            print(f'Ocurrió un error al eliminar el MenuDetalle: {e}')
             sys.exit()
-
-
-
